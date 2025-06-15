df6188d3a4e9ff3ab563a207938e928830061dd1
akilachamathha <akila@alphagenes.site>
2025-06-15 16:08:27 +0530

    style: Adjust padding in Footer component for improved layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ function Footer() {
       className="text-gray-300 py-12 px-4 sm:px-6 lg:px-8 font-sans"
       style={{ backgroundColor: "#1F2125" }}
     >
-      <div className="container mx-auto">
+      <div className="px-40 mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mb-10">
           {/* Map Section */}
           <div className="w-full md:col-span-1">
